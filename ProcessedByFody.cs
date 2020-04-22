4b7ad61cdd8aea0c6964b9132bf299fa0690c0f5
ulehvh <64109249+ulehvh@users.noreply.github.com>
2020-04-21 20:22:26 -0700

    Add files via upload

---
+++ ProcessedByFody.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// Token: 0x0200003A RID: 58
+internal class ProcessedByFody
+{
+	// Token: 0x04000178 RID: 376
+	internal const string FodyVersion = "6.0.0.0";
+
+	// Token: 0x04000179 RID: 377
+	internal const string Costura = "4.1.0.0";
+}
